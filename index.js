390366e39296fe33532bd8297cf9e95190d75b46
SaurabhKorde <99322733+SaurabhKorde@users.noreply.github.com>
2023-08-27 21:23:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+let name="Charuu";
+let num=100.11111111;
+let ishere=false
+let x;
+let y=null
+
+let car={
+    name:"bmw",
+    color:"blue"
+}
+
+let arr=[1,2,3,4,5,5]
+
+console.log(typeof(arr));
+
