ad4363daf29ffb389c96956b7084ba9676bc2d29
SaurabhKorde <99322733+SaurabhKorde@users.noreply.github.com>
2023-08-27 21:22:31 +0530

    Add files via upload

---
+++ conditional.js
@@ -0,0 +1,16 @@
+let age =10;
+
+// if(age>18){
+//     console.log("Eligible for Vote...");
+// }
+// else{
+//     console.log("Not Eligible for Vote...");
+// }
+
+//conditional Operator
+
+// condition ? value1(statement1) : value2(Statement2)
+
+let result= age>18 ?"Eligible for Vote..." :"Not Eligible for Vote...";
+
+console.log("Result:--",result);
