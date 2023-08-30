607ba273f6f2d7bcfa592def66b098db627ea203
SaurabhKorde <99322733+SaurabhKorde@users.noreply.github.com>
2023-08-30 22:04:16 +0530

    Add files via upload

---
+++ arrayMethods.js
@@ -0,0 +1,62 @@
+//Length Property
+// let names=["Raj","Deva","Aishwarya","Charmi","Ojas"];
+
+// console.log("Length Of names array:",names.length);
+// console.log("First Element:---",names[0]);
+// console.log("Last element:--",names[names.length-1]);
+
+//Push()
+
+// names.push("Kalyani");
+// names.push(11);
+// names.push("Ram");
+// console.log("Length Of names array:",names);
+
+//pop()
+// let names=["Raj","Deva","Aishwarya","Charmi","Ojas"];
+
+// delete names[names.length-1]
+
+// console.log(names.pop());
+// console.log("names array:",names);
+
+// //shift()
+// let names=["Raj","Deva","Aishwarya","Charmi","Ojas"];
+
+// // names.shift()
+// console.log(names.shift());
+// console.log("names array:",names);
+
+//unshift()
+// let names=["Raj","Deva","Aishwarya","Charmi","Ojas"];
+
+// names.unshift("Divya")
+// console.log("names array:",names);
+
+
+//Changing element
+// let names=["Raj","Deva","Aishwarya","Charmi","Ojas"];
+
+// console.log("names array:",names);
+
+// names[2]="Kalyani"
+// names[1]="Priti"
+
+// console.log("Changed array:",names);
+
+
+//Merging(Adding,Concatenating)
+
+// let boysName=["Sanket","Nikhil","Saket","Finney","Saurabh"];
+// let girlsName=["Aishu","Isha","Diksha","Sayli","Priti","Sayra"];
+// let number=[1,2,3,4,5,6]
+// console.log("boysName:---",boysName);
+// console.log("girlsName:--",girlsName);
+
+// let names=boysName.concat(girlsName,number);
+// console.log("Names:--",names);
+
+
+
+
+
---
+++ myArray.js
@@ -0,0 +1,11 @@
+
+let names=["Raj","Deva","Aishwarya","Charmi","Ojas"];
+
+let names2= new Array();
+
+names2[0]="Kartik"
+names2[1]="Sidhhi"
+
+let names3= new Array("Jay","Raghu","Radhika");
+
+console.log("Names 3:---",names3);
