0ba43f57f149520447f40b0fc34f446058aa2aba
SaurabhKorde <99322733+SaurabhKorde@users.noreply.github.com>
2023-08-31 21:47:04 +0530

    Add files via upload

--- arrayMethods.js
+++ arrayMethods.js
@@ -1,8 +1,9 @@
 //Length Property
-// let names=["Raj","Deva","Aishwarya","Charmi","Ojas"];
+//let names=["Raj","Deva","Aishwarya","Charmi","Ojas","Raj","Deva","Aishwarya","Charmi","Ojas","Raj","Deva","Aishwarya","Charmi","Ojas",1,2,34,5,6,6,7];
 
 // console.log("Length Of names array:",names.length);
-// console.log("First Element:---",names[0]);
+// // console.log("First Element:---",names[2]);
+
 // console.log("Last element:--",names[names.length-1]);
 
 //Push()
@@ -12,13 +13,15 @@
 // names.push("Ram");
 // console.log("Length Of names array:",names);
 
+
 //pop()
-// let names=["Raj","Deva","Aishwarya","Charmi","Ojas"];
-
+//let names=["Raj","Deva","Aishwarya","Charmi","Ojas"];
+               
 // delete names[names.length-1]
-
 // console.log(names.pop());
-// console.log("names array:",names);
+//console.log("names array:",names);
+
+
 
 // //shift()
 // let names=["Raj","Deva","Aishwarya","Charmi","Ojas"];
@@ -56,6 +59,40 @@
 // let names=boysName.concat(girlsName,number);
 // console.log("Names:--",names);
 
+//filter()
+
+// let number=[11,12,0,7,3,99,75,88,99,44,33,22,50,49,51];
+
+// const result=number.filter(function(num){
+//     return num<=50 });
+
+// console.log("number:--",number);
+// console.log("result:--",result);
+
+//entries()
+// let boysName=["Sanket","Nikhil","Saket","Finney","Saurabh"];
+
+// let bname=boysName.entries();
+// console.log("bname:--",bname);
+// for(let name of bname){
+//     console.log(name);
+// }
+
+
+let boysName=["Sanket","Nikhil","Saket","Finney","Saurabh"];
+
+// for(let i=0;i<boysName.length;i++){
+
+//     console.log("Name:---",boysName[i]);
+
+// }
+
+// let bname=boysName.map((n)=>{
+//     // return n;
+//     console.log(n);
+// })
+
+// console.log(bname);
 
 
 
